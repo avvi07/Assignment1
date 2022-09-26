8610000e437dbd68a23670929ac630c3e1fa2a50
avvi07 <114444916+avvi07@users.noreply.github.com>
2022-09-27 03:25:21 +0530

    Add files via upload

---
+++ 1Assignment.c
@@ -0,0 +1,138 @@
+// 1. Write a program to print Hello Students on the screen.
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello Students");
+//     return 0;
+// }
+
+// 2. Write a program to print Hello in the first line and Students in the second line.
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello\n");
+//     printf("Students");
+//     return 0;
+// }
+
+// 3. Write a program to print “MySirG” on the screen. (Remember to print in double quotes)
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"MySirG\"");
+//     return 0;
+// }
+
+// 4. WAP to find the area of the circle. Take radius of circle from user as input and print the 
+// result in below given format.
+
+// #include<stdio.h>
+// int main()
+// {
+//     float A;
+//     int R;
+//     float pi = 3.14;
+//     scanf("%d",&R);
+//     printf("%.4f",A =pi*R*R);
+//     return 0;
+// }
+
+// 5. WAP to calculate the length of String using printf function.
+
+// #include<stdio.h>
+// int main()
+// {
+//     char s[] ="Abhay";
+//     int i;
+
+//     for(i = 0; s[i] !='\0'; ++i);
+
+//     printf("Length of string: %d",i);
+//     return 0;
+// }
+
+// 6. WAP to print the name of the user in double quotes.
+
+// #include<stdio.h>
+// int main()
+// {
+//     char str[100];
+//     char a[100];
+
+//     scanf("%s%s",str,a);
+//     printf(" \"%s , %s\" ",str,a);
+//     return 0;
+// }
+
+// 7. WAP to print “%d” on the screen.
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("%%d");
+//     return 0;
+// }
+
+// 8. WAP to print “\n” on the screen.
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\n");
+//     return 0;
+// }
+
+// 9. WAP to print “\\” on the screen.
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\\\");
+//     return 0;
+// }
+
+// 10. WAP to take date as an input in below given format and convert the date format and 
+// display the result as given below.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int D;
+//     int M;
+//     int Y;
+
+//     printf("Ener the Dates:");
+//     scanf("\"%d" "/%d" "/%d\"",&D,&M,&Y);
+
+//     printf(" \"Day-%d , Month-%d , Year-%d\" ",D,M,Y);
+//     return 0;
+// }
+
+// 11. WAP to take time as an input in below given format and convert the time format and 
+// display the result as given below.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int M;
+//     int H;
+
+//     printf("Ener the Time: ");
+//     scanf("\"%d"":%d\"",&H,&M);
+
+//     printf(" \"%d Hour and %d Minute\" ",H,M);
+//     return 0;
+// }
+
+// 12. Find output of below code:
+
+// #include<stdio.h>
+// int main()
+// { 
+// int x = printf("ineuron");
+// printf("%d",x);
+// return 0;
+// }
